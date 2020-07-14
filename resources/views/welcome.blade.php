7f23a2f5ed6214cb8c4ed2508a1f8acabc3003fa
tianzijian <755965195@qq.com>
2020-07-14 16:50:50 +0800

    view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    api-1911
                 </div>
 
                 <div class="links">
